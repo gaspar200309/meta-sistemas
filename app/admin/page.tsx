0c6455a16870418908d3749083d77a3c7d552d1b
gaspar200309 <202201712@gmail.com>
2026-07-02 19:34:26 -0400

    fix: update routes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,9 @@
-import GamesSection from "@/components/landing/GamesSection";
 import Navbar from "@/components/landing/Navbar";
 
 export default function AdminPage() {
   return (
     <main className="min-h-screen bg-[#0b0202] text-white overflow-hidden">
       <Navbar />
-      <GamesSection juegoId={1} />
     </main>
   );
 }
